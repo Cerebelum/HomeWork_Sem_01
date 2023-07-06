d925748f20562d150ae0bea39cdbccfb3fbe05c7
Michael_Fesenko <@gmail.com>
2023-07-06 16:26:22 +0300

    добавил еще один вариант в Task_08

--- Task_08/Program.cs
+++ Task_08/Program.cs
@@ -23,7 +23,17 @@ else
 }
 
 // так компактнее
+// Console.Write("Введите число: ");
+// int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 // for (int i = 1; i < (num - 1); i++)
 //     if (i%2 == 0) Console.Write(i + ", ");
 // if (num%2 != 0) Console.Write(num - 1);
 // else Console.Write(num);
+
+// самый примитивный вариант
+// Console.Write("Введите число: ");
+// int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// if (num%2 != 0) num = num - 1;
+// for (int i = 2; i < num; i+=2)
+//     Console.Write(i + ", ");
+// Console.Write(num);
